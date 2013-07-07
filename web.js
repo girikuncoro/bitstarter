5e1924c243e7e871c4a15decc9f8b58dd40bea19
Ubuntu <ubuntu@ip-172-31-26-19.us-west-2.compute.internal>
2013-07-07 12:38:21 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require("fs");
   var buffer = new Buffer(32);
+
   response.send('Hello world 3!');
 });
 
