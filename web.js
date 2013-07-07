551761f1284f7758642c3c4c1ef9892645f0d0c0
Ubuntu <ubuntu@ip-172-31-26-19.us-west-2.compute.internal>
2013-07-07 12:39:54 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require("fs");
   var buffer = new Buffer(32);
+  console.log(fs.readFileSync("~/bitstarter/index.html"));
 
   response.send('Hello world 3!');
 });
